c18b6e0aee8d8315d9aef07a47ecb7d3fedd7fa7
jeng-yuantsai <tsaie79@gmail.com>
2023-11-16 18:34:27 +0000

    add commands

--- prom/run-grafana.sh
+++ prom/run-grafana.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -d --net=host --user "$(id -u)"\
- --volume "/workspaces/JIRIAF-test-platform/prom/grafana-data:/var/lib/grafana"\
+ --volume "/home/vscode/.prom-data/grafana-data:/var/lib/grafana"\
   grafana/grafana-enterprise
---
+++ prom/run-prometheus.sh
@@ -0,0 +1,6 @@
+docker run -d  \
+    --net=host \
+    --user "$(id -u)" \
+    -v /workspaces/JIRIAF-test-platform/prom:/prometheus \
+    -v /home/vscode/.prom-data/data:/prometheus/data \
+    prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.path=/prometheus/data 
