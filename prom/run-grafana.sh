7f2ef47bc6ff58148450b1a8c349e11991e90b77
jeng-yuantsai <tsaie79@gmail.com>
2023-11-16 18:38:57 +0000

    udate

--- prom/run-grafana.sh
+++ prom/run-grafana.sh
@@ -3,3 +3,5 @@
 docker run -d --net=host --user "$(id -u)"\
  --volume "/home/vscode/.prom-data/grafana-data:/var/lib/grafana"\
   grafana/grafana-enterprise
+
+  ## Manually open port 3000 in vscode
--- prom/run-prometheus.sh
+++ prom/run-prometheus.sh
@@ -4,3 +4,6 @@ docker run -d  \
     -v /workspaces/JIRIAF-test-platform/prom:/prometheus \
     -v /home/vscode/.prom-data/data:/prometheus/data \
     prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.path=/prometheus/data 
+
+
+      ## Manually open port 9090 in vscode
