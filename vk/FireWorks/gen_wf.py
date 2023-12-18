aa86b1e8786f34ffb2d144c7e19a3e476fb33655
tsaie79 <tsaie79@gmail.com>
2023-12-18 21:03:31 +0000

    Update queueadapter walltime and qos

--- vk/FireWorks/gen_wf.py
+++ vk/FireWorks/gen_wf.py
@@ -6,21 +6,21 @@ LPAD = LaunchPad.from_file('my_launchpad.yaml')
 def ersap_fw(nnode):
     fw_name = f"ersap-node{nnode}"
     
-    vk_string = f"#!/bin/bash\n\nexport NODENAME={fw_name}\nexport KUBECONFIG=/global/homes/j/jlabtsai/run-vk/kubeconfig/mylin\n\nssh -NfL 33469:localhost:33469 mylin\n\nshifter --image=docker:jlabtsai/vk-cmd:no-vk-container -- /bin/bash -c \"cp -r /vk-cmd `pwd`\"\n\ncd `pwd`/vk-cmd\n\n./start.sh $KUBECONFIG $NODENAME"
+    vk_string = f"#!/bin/bash\n\nexport NODENAME={fw_name}\nexport KUBECONFIG=/global/homes/j/jlabtsai/run-vk/kubeconfig/mylin\n\nssh -NfL 44875:localhost:44875 mylin\n\nshifter --image=docker:jlabtsai/vk-cmd:no-vk-container -- /bin/bash -c \"cp -r /vk-cmd `pwd`\"\n\ncd `pwd`/vk-cmd\n\n./start.sh $KUBECONFIG $NODENAME"
 
     task1 = ScriptTask.from_str(vk_string)
     
     fw = Firework([task1], name=fw_name)
     fw.spec["_category"] = "ersap-node1"
-    fw.spec["_queueadapter"] = {"job_name": fw_name, "walltime": "00:30:00",
-                                "qos": "debug_preempt", "nodes": 1}
+    fw.spec["_queueadapter"] = {"job_name": fw_name, "walltime": "01:00:00",
+                                "qos": "regular", "nodes": 1}
     # preempt has min walltime of 2 hours (can get stop after 2 hours)
     # debug_preempt has max walltime of 30 minutes (can get stop after 5 minutes)
     return fw
 
 
 def main():
-    for nnode in range(1, 9):
+    for nnode in range(1, 5):
         fw = ersap_fw(nnode)
         wf = Workflow([fw], {fw: []})
         wf.name = f"ersap-node{nnode}"
