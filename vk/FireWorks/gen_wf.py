fe26b4ff225f4e19073d8a6fadde8094dcdd7723
tsaie79 <tsaie79@gmail.com>
2023-12-19 07:00:49 +0000

    Add workflow launching script

--- vk/FireWorks/gen_wf.py
+++ vk/FireWorks/gen_wf.py
@@ -1,4 +1,6 @@
 from fireworks import Workflow, Firework, LaunchPad, ScriptTask, TemplateWriterTask
+import os
+import time
 
 
 LPAD = LaunchPad.from_file('my_launchpad.yaml')
@@ -12,20 +14,61 @@ def ersap_fw(nnode):
     
     fw = Firework([task1], name=fw_name)
     fw.spec["_category"] = "ersap-node1"
-    fw.spec["_queueadapter"] = {"job_name": fw_name, "walltime": "01:00:00",
+    fw.spec["_queueadapter"] = {"job_name": fw_name, "walltime": "00:30:00",
                                 "qos": "regular", "nodes": 1}
     # preempt has min walltime of 2 hours (can get stop after 2 hours)
     # debug_preempt has max walltime of 30 minutes (can get stop after 5 minutes)
     return fw
 
 
-def main():
-    for nnode in range(1, 5):
-        fw = ersap_fw(nnode)
-        wf = Workflow([fw], {fw: []})
-        wf.name = f"ersap-node{nnode}"
-        print(wf.as_dict())
-        LPAD.add_wf(wf)
+def get_wf(nnode):
+    fw = ersap_fw(nnode)
+    wf = Workflow([fw], {fw: []})
+    wf.name = f"ersap-node{nnode}"
+    return wf
+
+def add_wf():
+# this script is to launch the workflows based on:
+# 1. Constantly check if the latest added workflow is running
+# 2. If not, waith for 30 seconds and check again
+# 3. If yes, wait for 120 seconds and add the next workflow
+# 4. If the latest launched workflow is the last one, exit
+# 5. add the first workflow when no workflow is reserving or running
+    while True:
+        # get the list of workflows
+        wfs = LPAD.get_wf_ids()
+        wfs.sort()
+        if len(wfs) == 0:
+            print("No workflow is running or reserving, add the first workflow")
+            LPAD.add_wf(get_wf(1))
+            continue
+        # get the latest workflow
+        latest_wf = wfs[-1]
+        print(f"Latest workflow is {latest_wf}")
+        # get the status of the latest workflow
+        wf_status = LPAD.get_wf_summary_dict(latest_wf)["state"]
+        wf_name = LPAD.get_wf_summary_dict(latest_wf)["name"]
+        # if the latest workflow is not running, wait for 30 seconds and check again
+        while wf_status != "RUNNING":
+            time.sleep(3)
+            wf_status = LPAD.get_wf_summary_dict(latest_wf)["state"]
+            print(f"Workflow {wf_name} is in {wf_status} state")
+        # if the latest workflow is running, wait for 120 seconds and add the next workflow
+        # launch the pod tha has the label name of the wf name
+        print(f"Workflow {wf_name} is running; launch the pod {wf_name}")
+        os.system(f"kubectl apply -f /workspaces/JIRIAF-test-platform/vk/configmap/jobs/pod.yml -l app={wf_name}")
+        print("Wait for 120 seconds")
+        time.sleep(120)
+        # if the latest workflow is the last one, exit
+        if latest_wf == 8:
+            print("The last workflow {wf_name} is running; exit")
+            exit()
+        # add the next workflow
+        else:
+            next_wf = latest_wf + 1
+            LPAD.add_wf(get_wf(next_wf))
+            print(f"Add workflow {next_wf}")
+            # subprocess.run(["qlaunch", "-r", "singleshot", "-f", f"{next_wf.fws[-1].fw_id}"])
 
 if __name__ == "__main__":
-    main()
+    add_wf()
