8127dd693a2810890b393e2bd8072b1574534ea9
tsaie79 <tsaie79@gmail.com>
2024-01-07 18:51:58 +0000

    Remove unused scripts for Grafana and Prometheus

--- prom/run-server-no-dev-container/run-grafana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d --net=host --user "$(id -u)"\
- --volume "/home/tsai/JIRIAF/prom-data/grafana-data:/var/lib/grafana"\
-  grafana/grafana-enterprise
-
-  ## Manually open port 3000 in vscode
