d8f62acf335ed669a8d9f94ddd6305a078b6b7eb
tsaie79 <tsaie79@gmail.com>
2024-01-07 18:51:44 +0000

    Update file paths in run-prometheus.sh script

--- prom/run-prometheus.sh
+++ prom/run-prometheus.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+
+export HOST_HOME="/home/jeng-yuantsai"
+
+
 docker run -d \
+    --name prometheus \
     --net=host \
     --user "$(id -u)" \
-    -v /workspaces/JIRIAF-test-platform/prom:/prometheus \
-    -v /home/vscode/.prom-data/data:/prometheus/data \
+    -v $HOST_HOME/JIRIAF/JIRIAF-test-platform/prom:/prometheus \
+    -v $HOST_HOME/JIRIAF/prom-data/data:/prometheus/data \
     prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.path=/prometheus/data 
 
 
