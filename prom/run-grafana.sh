858c10f12005ea24e49c6a6e0d37698379504c64
tsaie79 <tsaie79@gmail.com>
2024-01-07 18:51:40 +0000

    Update Grafana data volume path

--- prom/run-grafana.sh
+++ prom/run-grafana.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+export HOST_HOME="/home/jeng-yuantsai"
+
 docker run -d --net=host --user "$(id -u)"\
- --volume "/home/vscode/.prom-data/grafana-data:/var/lib/grafana"\
+ --name=grafana\
+ --volume $HOST_HOME/JIRIAF/prom-data/grafana-data:/var/lib/grafana\
   grafana/grafana-enterprise
 
   ## Manually open port 3000 in vscode
