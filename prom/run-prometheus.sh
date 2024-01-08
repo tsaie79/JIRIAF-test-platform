60dbfd894ee43d1eeea9688c16c1ef9826779d2e
tsaie79 <tsaie79@gmail.com>
2024-01-08 11:15:07 -0500

    add data retension day to 720d

--- prom/run-prometheus.sh
+++ prom/run-prometheus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-export HOST_HOME="/home/jeng-yuantsai"
+export HOST_HOME="/home/tsai"
 
 
 docker run -d \
@@ -10,7 +10,7 @@ docker run -d \
     --user "$(id -u)" \
     -v $HOST_HOME/JIRIAF/JIRIAF-test-platform/prom:/prometheus \
     -v $HOST_HOME/JIRIAF/prom-data/data:/prometheus/data \
-    prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.path=/prometheus/data 
+    prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.path=/prometheus/data --storage.tsdb.retention.time=720d
 
 
       ## Manually open port 9090 in vscode
