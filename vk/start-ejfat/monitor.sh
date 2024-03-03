c6db7ccc0be7aaeb6d854fd718117e404dd3fffd
tsaie79 <tsaie79@gmail.com>
2024-03-03 17:37:50 +0000

    Update monitor.sh script to include additional commands for checking ports with lsof

--- vk/start-ejfat/monitor.sh
+++ vk/start-ejfat/monitor.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 while true; do
-    kubectl get pods -o wide
-    echo ---
+    echo "=========="
     kubectl top node
-    echo ---
+    # echo ---
+    # kubectl -n ersap-1 get pods -o wide
+    # echo ****
+    # kubectl -n ersap-2 get pods -o wide
+    echo "---"
     kubectl top pods
+    # echo "---"
+    # kubectl -n "2" top pods
     sleep 3
+done
+
+
+
+# check the ports 2776, 2321, 46859, 10250 with lsof 
+for port in 1776 12345 2221 46859 10250; do
+    lsof -i :$port
 done
