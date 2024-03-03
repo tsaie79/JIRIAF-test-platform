5544678ae498b2c47b9c33bddc85b23ba9a45bc7
tsaie79 <tsaie79@gmail.com>
2024-03-03 17:37:54 +0000

    Add SSH port forwarding for process-exporter and stress

--- vk/start-ejfat/ssh.sh
+++ vk/start-ejfat/ssh.sh
@@ -10,13 +10,23 @@ ssh -NfR $APISERVER_PORT:localhost:$APISERVER_PORT ejfat-fs
 export KUBELET_PORT="10250"
 ssh -NfL *:$KUBELET_PORT:localhost:$KUBELET_PORT ejfat-fs
 
+
+
+#prom process-exporter ersap-1
+export PROCESS_EXPORTER_PORT="1776"
+ssh -NfL $PROCESS_EXPORTER_PORT:localhost:$PROCESS_EXPORTER_PORT ejfat-fs 
+
+# k8s: stress
+export STRESS_PORT="12345"
+ssh -NfL $STRESS_PORT:localhost:$STRESS_PORT ejfat-fs
+
 # prom ersap
+## 4 pipe
 for i in {2221..2224}
 do
     j=$((i + 10000))
     ssh -NfL $j:localhost:$i ejfat-fs
 done
 
-#prom process-exporter
-export PROCESS_EXPORTER_PORT="12345"
-ssh -NfL $PROCESS_EXPORTER_PORT:localhost:$PROCESS_EXPORTER_PORT ejfat-fs 
+## 1 pipe
+ssh -NfL 2221:localhost:2221 ejfat-fs
